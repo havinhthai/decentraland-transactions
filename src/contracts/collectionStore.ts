226f9f38a98c8ae364017698d1a679c2b90f36fe
Kyllian LE BORGNE ROEPRCH <me@hprivakos.net>
2021-06-01 15:51:36 +0200

    fix: add matic mainnet collectionstore

--- src/contracts/collectionStore.ts
+++ src/contracts/collectionStore.ts
@@ -8,5 +8,12 @@ export const collectionStore = {
     address: '0x6ddF1b1924DAD850AdBc1C02026535464Be06B0c',
     name: 'Decentraland Collection Store',
     chainId: ChainId.MATIC_MUMBAI
+  },
+  [ChainId.MATIC_MAINNET]: {
+    version: '1',
+    abi: abis.CollectionStore,
+    address: '0x843b56E58949e71D480Ef9C4F9b0F3304Ea9927C',
+    name: 'Decentraland Collection Store',
+    chainId: ChainId.MATIC_MAINNET
   }
 }
